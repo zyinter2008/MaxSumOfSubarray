ee70e1d77bd8ed60450122198d93893ab893f959
zyin <zyin@zyindeMacBook-Pro.local>
2017-03-18 10:55:38 +0800

    refact

--- src/MaxSumOfSubarray.cpp
+++ src/MaxSumOfSubarray.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int calcMaxSumOfSubarray(int *array, int size);
+
 int main() {
 
-	int array[8] = {1, -2, 3, 10, -4, 7, 2, -5};
+	int array[8] = { 1, -2, 3, 10, -4, 7, 2, -5 };
+	int maxSum = calcMaxSumOfSubarray(array, 8);
+	cout << "max sum:" << maxSum << endl;
+	return 0;
+}
 
+int calcMaxSumOfSubarray(int *array, int size) {
 	int maxSum = 0, currentSum = 0, maxElement = array[0];
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < size; i++) {
 		if (maxElement < array[i]) {
 			maxElement = array[i];
 		}
@@ -16,15 +23,13 @@ int main() {
 			currentSum = 0;
 		}
 
-		if(currentSum > maxSum){
+		if (currentSum > maxSum) {
 			maxSum = currentSum;
 		}
 	}
 
-	if(maxElement < 0){
+	if (maxElement < 0) {
 		maxSum = maxElement;
 	}
-
-	cout << "max sum:" << maxSum << endl; // prints !!!Hello World!!!
-	return 0;
+	return maxSum;
 }
